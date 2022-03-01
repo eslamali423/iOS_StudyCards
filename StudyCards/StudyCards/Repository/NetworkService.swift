9a8d5799dec2724f539acfe60b053ec5ec185218
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-01 18:36:37 +0200

    create Repository Class

---
+++ StudyCards/StudyCards/Repository/NetworkService.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkService.swift
+//  StudyCards
+//
+//  Created by Eslam Ali  on 01/03/2022.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine
+
+class NetworkService : ObserverObject {
+    
+    static let shared =  NetworkService()
+   
+    let store = Firestore.firestore()
+    var cards : [StudyCard] = []
+    
+    func get()  {
+        
+            
+            
+            
+            
+//
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+    }
+    
+
---
+++ StudyCards/StudyCards/Repository/Repository.swift
@@ -0,0 +1,20 @@
+//
+//  Repository.swift
+//  StudyCards
+//
+//  Created by Eslam Ali  on 01/03/2022.
+//
+
+import Foundation
+
+class Repository : ObservableObject{
+    
+    func get()  {
+        NetworkManager.shared.get()
+    }
+    func add(studyCard : StudyCard)  {
+        NetworkManager.shared.add(studyCard: studyCard)
+    }
+    
+    
+}
